f0a7ebd442f8f7e250d313a222f41b5f125fc023
yuchensun97 <sunyuchen.fs@hotmail.com>
2020-04-06 00:26:01 -0400

    implement weight initialization

--- code/model.py
+++ code/model.py
@@ -1,10 +1,12 @@
 '''
-This script define the A3C model
+This script define the A3C architecture
 '''
 
+import torch.nn.functional as F
 import torch.nn as nn
 import numpy as np
 import random
+from utils import init_weight
 
 class A3C(nn.Module):
     def __init__(self, num_state, num_action):
@@ -12,22 +14,34 @@ class A3C(nn.Module):
         Build your nn architecture here
         """
         super(A3C,self).__init__()
-        pass
+        self.num_state = num_state
+        self.num_action = num_action
+        self.conv1 = nn.Conv2d(num_state,32,3,stride=1,padding=1)
+        self.conv2 = nn.Conv2d(32,32,3,stride=1,padding=1)
+        self.conv3 = nn.Conv2d(32,32,3,stride=1,padding=1)
+        self.conv4 = nn.Conv2d(32,32,3,stride=1,padding=1)
+        self.mp = nn.MaxPool2d((2,2))
+        self.bn = nn.BatchNorm2d(32)
+        self.lstm = nn.LSTMCell(32*8*8,512)
+        self.actor = nn.Linear(512,num_action)
+        self.critic = nn.Linear(512,1)
+        init_weight([self.conv1,self.conv2,self.conv3,self.conv4,self.lstm,self.actor,self.critic])
 
-    def forward(self,x):
+    def forward(self,x,hx,cx):
         """
         Forward
         """
-        pass
-
-    def choose_action(self,state):
-        """
-        choose action based on the probability or argmax
-        """
-        pass
-
-    def loss(self, state,action,value):
-        """
-        compute loss
-        """
-        pass
+        x = F.relu(self.conv1(x))
+        x = self.mp(x)
+        x = self.bn(x)
+        x = F.relu(self.conv2(x))
+        x = self.mp(x)
+        x = self.bn(x)
+        x = F.relu(self.conv3(x))
+        x = self.mp(x)
+        x = self.bn(x)
+        x = F.relu(self.conv4(x))
+        x = self.mp(x)
+        x = self.bn(x)
+        hx,cx = self.lstm(x.view(x.size(0),-1),(hx,cx))
+        return self.actor(hx),self.critic(hx),hx,cx
--- code/utils.py
+++ code/utils.py
@@ -1,8 +1,9 @@
 """
-This script define the memory for the agent to remember its experience
+This script define the helper function for the agent to use
 """
 import numpy as np
 from collections import deque 
+import torch.nn as nn
 class Memory:
     def __init__(self,max_size):
         self.buffer = deque(maxlen=max_size)
@@ -14,3 +15,12 @@ class Memory:
         buffer_size = len(self.buffer)
         idx = np.random.choice(np.arange(buffer_size),size=batch_size,replace=False)
         return [self.buffer[i] for i in idx]
+
+def init_weight(layers):
+    for layer in layers:
+        if type(layer) == nn.Conv2d or type(layer)==nn.Linear:
+            nn.init.xavier_uniform_(layer.weight) # initialize weights
+            nn.init.constant_(layer.bias,0)    # bias set to 0
+        elif type(layer) == nn.LSTMCell:
+            nn.init.constant_(layer.bias_ih,0)
+            nn.init.constant_(layer.bias_hh,0) 
