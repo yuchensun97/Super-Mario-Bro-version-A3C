53619d95e807be1b819d4fbe007c7bacaf806c77
yuchensun97 <sunyuchen.fs@hotmail.com>
2020-04-10 01:12:27 -0400

    update agent

--- code/agent.py
+++ code/agent.py
@@ -8,13 +8,81 @@ import numpy as np
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import RIGHT_ONLY, SIMPLE_MOVEMENT, COMPLEX_MOVEMENT
 from nes_py.wrappers.joypad_space import JoypadSpace
+from gym import spaces
+from gym.spaces.box import Box
+from gym import Wrapper
 from collections import deque    # ordered collections
 
 import torch
+import torch.multiprocessing as mp
 
 # hyper parameters
 from params import *
 from model import A3C
-from utils import Memory
+from utils import Memory,preprocess,video
 from optimizer import Adam_global
-from env import preprocess, video
+
+class Reward(Wrapper):
+    def __init__(self,env):
+        """
+        Args:
+            env -- the gym environment passed in
+        """
+        super(Reward).__init__(env)
+        self.observation_space = Box(low=0,high=255,shape=(1,128,128))    # define observation space
+        self.curr_score = 0
+
+    def step(self,action):
+        """
+        update step reward here
+        """
+        state,reward,done,info = self.env.step(action)    # obtain the 
+        state = preprocess(state)
+        reward+=(info['score']-self.curr_score)/50
+        self.curr_score = info['score']
+        if done:
+            if info['flag_get']:
+                reward+=50
+            else:
+                reward-=50
+        return state, reward,done,info
+
+    def reset(self):
+        """
+        reset 
+        """
+        state = self.env.reset()
+        self.curr_score = 0
+        return preprocess(state)
+
+class SkipEnv(Wrapper):
+    def __init__(self,env,skip=4):
+        """
+        Return only every 'skip' frame
+        Default skip frames: 4
+        """
+        super(SkipEnv,self).__init__(env)
+        self.observation_space = Box(low=0,high=255,shape=(4,128,128))
+        self.skip = skip
+        self.skip_frame = deque(maxlen=skip)    # buffer to store skip observation
+
+    def step(self,action):
+        skip_reward = 0.0
+        done = None
+        for _ in range(self.skip):
+            state,reward,done,info = self.env.step(action)
+            self.skip_frame.append(state)
+            skip_reward+=reward
+            if done:
+                break
+        states = np.stack(self.skip_frame,axis=0)
+        return states.astype(np.float32),reward,done,info
+
+    def reset(self):
+        """
+        clear past frame buffer and init to first state
+        """
+        self.skip_frame.clear()
+        state = self.env.reset()
+        self.skip_frame.append(state)
+        return state
--- code/utils.py
+++ code/utils.py
@@ -4,6 +4,11 @@ This script define the helper function for the agent to use
 import numpy as np
 from collections import deque 
 import torch.nn as nn
+
+import cv2
+
+from params import *
+
 class Memory:
     def __init__(self,max_size):
         self.buffer = deque(maxlen=max_size)
@@ -24,3 +29,35 @@ def init_weight(layers):
         elif type(layer) == nn.LSTMCell:
             nn.init.constant_(layer.bias_ih,0)
             nn.init.constant_(layer.bias_hh,0) 
+
+def preprocess(frame):
+    if frame is not None:
+        frame = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)   # convert to grey scale to improve trainning speed
+        frame = cv2.resize(frame,(128,128))[None,:,:]/255.
+    else:
+        return np.zeros((1,128,128))
+
+def video(video_frame,state,new_episode_bool):
+    """
+    Args:
+        video_frame -- a deque object used to store frames
+        state -- agent's observation
+        new_episode_bool -- a boolean used to determine a new episode
+    """
+    frame = preprocess(state)
+
+    if new_episode_bool:
+        # clear stacked frames
+        stacked_frame = deque([np.zeros((128,128),dtype=np.int) for i in range(stack_size)],maxlen=4)
+        stacked_frame.append(frame)
+        stacked_frame.append(frame)
+        stacked_frame.append(frame)
+        stacked_frame.append(frame)
+
+        stacked_state = np.stack(stacked_frame,axis=2)
+    else:
+        # append frame to deque
+        stacked_frame.append(frame)
+        stacked_state = np.stack(stacked_frame,axis=2)
+
+    return stacked_state,stacked_frame
